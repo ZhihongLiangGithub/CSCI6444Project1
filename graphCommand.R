b39d1717257df0f66471746ffa537dbe296f9f1a
Ziyi <slcfv3@gmail.com>
2017-06-15 20:20:42 -0400

    average and simplified.

--- graphCommand.R
+++ graphCommand.R
@@ -1,4 +1,12 @@
 library(igraph)
-g <-graph.edgelist(as.matrix(newdf3), directed=TRUE)
+g <-graph.edgelist(as.matrix(newdf3), directed = TRUE)
 x <-get.adjacency(g)
-print(x)
+newGraph<- graph.adjacency(x, mode = "directed", weighted = TRUE)
+newGraph <-simplify(newGraph, remove.multiple = TRUE, remove.loops = TRUE,
+         edge.attr.comb = igraph_opt("edge.attr.comb"))
+wei<-E(newGraph)$weight
+mean(wei)
+newGraph <-delete.edges(newGraph, which(E(newGraph)$weight<=23))
+finalGraph<- delete.vertices(newGraph,which(degree(newGraph)<1))
+plot(g)
+
