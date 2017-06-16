cadcfb12beee84d3b08c29936ca5eaac1d46977f
Zhihong <zhihongliang@gwmail.gwu.edu>
2017-06-15 20:48:19 -0400

    doc update

--- explore_liang.R
+++ explore_liang.R
@@ -11,7 +11,7 @@ murg <- delete.edges(rg, which(!is.mutual(rg)))
 plot(murg, layout = l)
 
 # 2. BFS search of a graph -- graph.bfs()
-print(graph.bfs(rg, V(rg)[1]))
+print(graph.bfs(rg, V(rg)[1], father = T, pred = T, succ = T))
 
 # 3. DFS search of a graph -- graph.dfs()
-print(graph.dfs(rg, V(rg)[1]))
+print(graph.dfs(rg, V(rg)[1], father = T, pred = T, succ = T))
