453842064dac3b45a666986f57aebad4a2fe6f22
Ziyi Wang <slcfv3@gmail.com>
2017-06-15 20:35:22 -0400

    Update graphCommand.R

--- graphCommand.R
+++ graphCommand.R
@@ -5,8 +5,8 @@ newGraph<- graph.adjacency(x, mode = "directed", weighted = TRUE)
 newGraph <-simplify(newGraph, remove.multiple = TRUE, remove.loops = TRUE,
          edge.attr.comb = igraph_opt("edge.attr.comb"))
 wei<-E(newGraph)$weight
-mean(wei)
-newGraph <-delete.edges(newGraph, which(E(newGraph)$weight<=23))
+ave<-mean(wei)
+newGraph <-delete.edges(newGraph, which(E(newGraph)$weight<=ave))
 finalGraph<- delete.vertices(newGraph,which(degree(newGraph)<1))
 plot(finalGraph)
 
